0651a78827c453655aca0b62dbf4c46ecfcd7ce0
carolina234 <96142611+carolina234@users.noreply.github.com>
2022-02-14 10:32:13 -0300

    Update scrips.js

--- scrips.js
+++ scrips.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 // data do lançamento: 11/03/2022
 const dataDoLancamento = new Date(2022, 02, 02)
 
@@ -49,55 +47,3 @@ function fazerContagemRegressiva() {
 }
 
 var ana = setInterval(fazerContagemRegressiva, 1000)
-
-
-  
-    
-=======
-
-
-// data do lançamento: 02/03/2022
-const dataDoLancamento = new Date(2022, 02, 02)
-
-// Variáveis: ms = milissegundos
-const segundoEmMs = 1000
-const minutoEmMs = segundoEmMs * 60
-const horaEmMs = minutoEmMs * 60
-const diaEmMs = horaEmMs * 24
-
-// Funcionalidade
-function fazerContagemRegressiva() {
-  const diaDeHoje = new Date()
-
-  const tempoRestanteParaOLancamentoEmMs = dataDoLancamento - diaDeHoje
-
-  // converter milissegundos para o seu respectivo tempo
-  const dias = String(Math.floor(tempoRestanteParaOLancamentoEmMs / diaEmMs))
-  const horas = String(
-    Math.floor((tempoRestanteParaOLancamentoEmMs % diaEmMs) / horaEmMs)
-  )
-  const minutos = String(
-    Math.floor((tempoRestanteParaOLancamentoEmMs % horaEmMs) / minutoEmMs)
-  )
-  const segundos = String(
-    Math.floor((tempoRestanteParaOLancamentoEmMs % minutoEmMs) / segundoEmMs)
-  )
-
-  document.querySelector('div').textContent = `${dias} : ${horas.padStart(
-    2,
-    '0'
-  )}: ${minutos.padStart(2, '0')} : ${segundos.padStart(2, '0')}`
-  if (tempoRestanteParaOLancamentoEmMs < 0) {
-    clearInterval(set);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-
-  }
-
-
- const set = setInterval(fazerContagemRegressiva, 1000)
-
-
-  
-    
->>>>>>> 8bea9db38e492c684a0e00cc5d45ed2e9d72d307
